6f99c9bdb4d7f38d54c7d853ecc61faf4ffb5e98
Maksim Stseshanok <maksim.stseshanok@gmail.com>
2021-01-21 21:58:54 +0300

    JDBC + SELECT

---
+++ src/Dao.java
@@ -0,0 +1,9 @@
+public interface Dao {
+
+/*   boolean create(User user);
+   User read(String login, String pass);
+   User update(String login, User  user);
+   boolean delete(String login, String pass);
+   List<User> readAll();
+   */
+}
--- src/by/itacademy/Main.java
+++ src/by/itacademy/Main.java
@@ -1,33 +1,36 @@
 package by.itacademy;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         String url = "jdbc:mysql://127.0.0.1:3307/app";
-        String username = "root";
+        String username = "root";                                                                                             //Это драйвер в оттдельном классе
         String password = "root";
         Class.forName("com.mysql.jdbc.Driver");
 
         try(Connection conn = DriverManager.getConnection(url, username, password)){
             System.out.println("successful");
-            //PreparedStatement stmt = conn.prepareStatement("UPDATE user SET login=?, password=? WHERE id=?");
-            //PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (login, password) VALUES (?,?)");
+            //PreparedStatement stmt = conn.prepareStatement("UPDATE user SET login=?, password=? WHERE id=?");                   Это реализация
+            PreparedStatement stmt = conn.prepareStatement("SELECT password FROM users where  login=?");
+            //PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (login, password) VALUES (?,?)");
             //PreparedStatement stmt = conn.prepareStatement("DELETE FROM user WHERE");
-            stmt.setString(1, "Roman");
-            stmt.setString(2, "232323");
+            //stmt.setString(1, "Roman");
+            //stmt.setString(2, "232323");
             //stmt.setInt(3, 3);
-            stmt.execute();
-            //String name;
-            /*while(rs.next()){
+
+
+            //Для SELECT
+            ResultSet rs = stmt.executeQuery();
+            String name;
+            while(rs.next()){
                 name = rs.getString("password");
                 System.out.println("--------------------");
-                System.out.println("name+"/n);
-            }*/
+                System.out.println(name+"\n");
+            }
+            //Для SELECT
+            stmt.execute();
+
         } catch (SQLException e){
             e.printStackTrace();
         }
